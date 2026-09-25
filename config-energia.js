4d221fd46add0d9033f0f86d4bb61ccb20e94d61
Leticia Freitas <104323257+lelefrei@users.noreply.github.com>
2026-09-25 13:01:05 -0300

    Delete config-energia.js

--- config-energia.js
+++
@@ -1,5 +0,0 @@
-
-window.SIQA_CONFIG = {
-  SUPABASE_URL: "https://avgqufnzbqlahxnxqwjw.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2Z3F1Zm56YnFsYWh4bnhxd2p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNDM3NzMsImV4cCI6MjEwNTkxOTc3M30.lX8aXXEz2BoN_grQvOya6i0iPbfG_3fAPNmM00dFxpU"
-};
