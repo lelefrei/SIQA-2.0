2401a7fb91e90123604a502d3d7ac4f4903d05fc
Leticia Freitas <104323257+lelefrei@users.noreply.github.com>
2026-09-25 12:43:54 -0300

    config-energia.js

--- config-energia.js
+++ config-energia.js
@@ -6,6 +6,6 @@
  * Não coloque a senha do PostgreSQL local neste arquivo.
  */
 window.SIQA_CONFIG = {
-  SUPABASE_URL: "COLE_AQUI_A_URL_DO_SEU_SUPABASE",
-  SUPABASE_ANON_KEY: "COLE_AQUI_A_CHAVE_ANON_DO_SEU_SUPABASE"
+  SUPABASE_URL: "https://avgqufnzbqlahxnxqwjw.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2Z3F1Zm56YnFsYWh4bnhxd2p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNDM3NzMsImV4cCI6MjEwNTkxOTc3M30.lX8aXXEz2BoN_grQvOya6i0iPbfG_3fAPNmM00dFxpU"
 };
